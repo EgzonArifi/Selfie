fa425fdc5813d8b769fa01345bc72ae5fc6977c7
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-19 18:18:56 +0200

    created project

---
+++ Selfie/Selfie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/19/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Selfie/Selfie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/19/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
