2166ba04eccff5f84b1e0f346774f31e9f93c0fe
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-20 00:56:51 +0200

    created sticker cell

---
+++ Selfie/Selfie/StickerCell.h
@@ -0,0 +1,14 @@
+//
+//  StickerCell.h
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/20/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StickerCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *stickerImageView;
+
+@end
---
+++ Selfie/Selfie/StickerCell.m
@@ -0,0 +1,18 @@
+//
+//  StickerCell.m
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/20/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import "StickerCell.h"
+
+@implementation StickerCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
