afb8fd4e7c0204d3dee802ff820b36af74c55ddc
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-21 15:18:38 +0200

    created candidate model

---
+++ Selfie/Selfie/CandidateModel.h
@@ -0,0 +1,13 @@
+//
+//  CandidateModel.h
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/21/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CandidateModel : NSObject
+
+@end
---
+++ Selfie/Selfie/CandidateModel.m
@@ -0,0 +1,13 @@
+//
+//  CandidateModel.m
+//  Selfie
+//
+//  Created by Egzon Arifi on 5/21/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import "CandidateModel.h"
+
+@implementation CandidateModel
+
+@end
