d98b03d046892103737b576ff713e2d2b4659b69
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-24 23:02:56 +0200

    pods , test app icon

---
+++ Selfie/Pods/Target Support Files/Pods-Selfie me LDK/Pods-Selfie me LDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Selfie_me_LDK : NSObject
+@end
+@implementation PodsDummy_Pods_Selfie_me_LDK
+@end
---
+++ Selfie/Pods/Target Support Files/Pods-Selfie me LDKTests/Pods-Selfie me LDKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Selfie_me_LDKTests : NSObject
+@end
+@implementation PodsDummy_Pods_Selfie_me_LDKTests
+@end
