b885dd61978c5b0057142983212d635ec8e054eb
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-05-27 21:42:14 +0200

    cell camera sticker

---
+++ Selfie/Selfie/CameraCollectionStikcerCell.h
@@ -0,0 +1,14 @@
+//
+//  CameraCollectionStikcerCell.h
+//  Selfie me LDK
+//
+//  Created by Egzon Arifi on 5/24/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraCollectionStikcerCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *sticker;
+@end
---
+++ Selfie/Selfie/CameraCollectionStikcerCell.m
@@ -0,0 +1,13 @@
+//
+//  CameraCollectionStikcerCell.m
+//  Selfie me LDK
+//
+//  Created by Egzon Arifi on 5/24/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import "CameraCollectionStikcerCell.h"
+
+@implementation CameraCollectionStikcerCell
+
+@end
