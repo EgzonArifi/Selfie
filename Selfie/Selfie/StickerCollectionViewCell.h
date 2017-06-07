363855d414c8d3b7fba192d6a7dac39678141a4c
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2017-06-07 17:24:09 +0200

    implemented sticker collection view cell

---
+++ Selfie/Selfie/StickerCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  StickerCollectionViewCell.h
+//  Selfie me LDK
+//
+//  Created by Egzon Arifi on 5/24/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StickerCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *sticker;
+
+@end
---
+++ Selfie/Selfie/StickerCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  StickerCollectionViewCell.m
+//  Selfie me LDK
+//
+//  Created by Egzon Arifi on 5/24/17.
+//  Copyright © 2017 Egzon Arifi. All rights reserved.
+//
+
+#import "StickerCollectionViewCell.h"
+
+@implementation StickerCollectionViewCell
+
+@end
